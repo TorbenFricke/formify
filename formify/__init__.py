fe04b1564992ad6b3bfdba363d35bcdde1aba456
Torben <mail@torben.co>
2020-09-16 17:44:48 +0200

    windows css

--- formify/__init__.py
+++ formify/__init__.py
@@ -3,11 +3,23 @@ from formify.window import MainWindow
 
 import pathlib
 
-stylesheet_path = str(pathlib.Path(__file__).parent / "style.css")
+stylesheet_root = pathlib.Path(__file__).parent
 
 def stylesheet() -> str:
-	with open(stylesheet_path, "r") as f:
-		return f.read()
+	def read(css_file):
+		with open(str(stylesheet_root / css_file), "r") as f:
+			return f.read()
+
+	css = read("style.css")
+	css += "\n"
+
+	# platform specific css
+	import os
+	# windows
+	if os.name == "nt":
+		css += read("windows.css")
+
+	return css
 
 from PySide2 import QtWidgets
 import sys
