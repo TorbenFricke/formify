fdcdeb3a3515d95c824530f092bb1ac18bd7f64e
Torben <mail@torben.co>
2020-09-16 15:35:17 +0200

    added setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+setup(
+    name="formify",
+    version="0.1.0",
+    packages=find_packages(),
+	package_data={
+        "": ["*.css"],
+    },
+	install_requires=["pyside2", "matplotlib"]
+)
