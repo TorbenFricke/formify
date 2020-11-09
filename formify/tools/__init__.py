5f727d1507dc1821934d07c5b784834c03893bc0
Torben Fricke <torben.fricke@ial.uni-hannover.de>
2020-11-09 13:39:56 +0100

    minor changes

--- formify/tools/__init__.py
+++ formify/tools/__init__.py
@@ -1,4 +1,9 @@
-from formify.tools.background_method import BackgroundMethod, Task
+from formify.tools.background_method import BackgroundMethod, Task, do_in_ui_thread
 from formify.tools.relationship import Relationship
 from formify.tools.file_dialogs import save_dialog, open_dialog
+from PySide2 import QtWidgets
 
+
+def maximize(widget) -> QtWidgets.QWidget:
+	widget.setSizePolicy(QtWidgets.QSizePolicy.Expanding, QtWidgets.QSizePolicy.Expanding)
+	return widget
