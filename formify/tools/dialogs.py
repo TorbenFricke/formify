806b2bec1300133992b1eeff866d652d5e9da1b8
Torben Fricke <torben.fricke@ial.uni-hannover.de>
2020-11-11 12:21:05 +0100

    dialogs

--- formify/tools/dialogs.py
+++ formify/tools/dialogs.py
@@ -4,6 +4,7 @@ from formify.controls import ControlMessagebox
 def yes_no_dialog(title="Sure?", text="Are you sure?"):
 	return ControlMessagebox(title, text).show()
 
+
 def yes_no_abort_dialog(title="Sure?", text="Are you sure?"):
 	class YesNoAbort(ControlMessagebox):
 		def make_buttons(self):
@@ -15,7 +16,7 @@ def yes_no_abort_dialog(title="Sure?", text="Are you sure?"):
 	return YesNoAbort(title, text).show()
 
 
-def ok_dialog(title="Information", text="Something is going to happen"):
+def ok_dialog(title="Information", text="Something is going to happen!"):
 	class Ok(ControlMessagebox):
 		def make_buttons(self):
 			self.setStandardButtons(self.Ok)
