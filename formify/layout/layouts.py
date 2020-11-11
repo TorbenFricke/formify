960a1f8f03eeba7c6a892efdab2cca7c06b68002
Torben Fricke <torben.fricke@ial.uni-hannover.de>
2020-11-11 12:31:50 +0100

    refactored layouts.py

--- formify/layout/layouts.py
+++ formify/layout/layouts.py
@@ -29,81 +29,34 @@ def add_layout_or_widget(parent, child):
 		parent.addLayout(child)
 
 
-class BaseLayout:
-	def __init__(self, *args, **kwargs):
-		self.kwargs = kwargs
-		self.controls = args
+def _formset_layout(layout, controls):
+	layout.setMargin(0)
+	for control in controls:
+		layout.addWidget(ensure_widget(control))
+	return layout
 
-	def _make_layout(self) -> QtWidgets.QLayout:
-		return QtWidgets.QVBoxLayout()
-
-	@staticmethod
-	def formset_children(layout: QtWidgets.QLayout, controls, parent, children_controls):
-		for item in controls:
-
-			# another nested layout element
-			if isinstance(item, QtWidgets.QLayout):
-				layout.addWidget(ensure_widget(item))
-
-			# a formify ControlBase
-			elif isinstance(item, ControlBase):
-				layout.addWidget(item)
-
-			# a nested QWidget - For example a label
-			# Note: The order is important, as sometimes Controls subclass ControlBase and QWidget.
-			#       In these cases we want to execute the previous elif block.
-			elif isinstance(item, QtWidgets.QWidget):
-				layout.addWidget(item)
-
-	def generate(self, parent=None, children_controls: list=None) -> typing.Union[QtWidgets.QLayout, QtWidgets.QWidget]:
-		layout = self._make_layout()
-		layout.setMargin(0)
-		self.formset_children(layout, self.controls, parent=parent, children_controls=children_controls)
-		return layout
+def Row(*controls) -> QtWidgets.QLayout:
+	layout = QtWidgets.QHBoxLayout()
+	_formset_layout(layout, controls)
+	return layout
 
 
-class _Row(BaseLayout):
-	def _make_layout(self):
-		return QtWidgets.QHBoxLayout()
-
-def Row(*args, **kwargs) -> QtWidgets.QLayout:
-	return _Row(*args, **kwargs).generate()
-
-
-class _Column(BaseLayout):
-	def _make_layout(self):
-		layout = QtWidgets.QVBoxLayout()
-		layout.setAlignment(QtCore.Qt.AlignTop)
-		return layout
-
-def Col(*args, **kwargs) -> QtWidgets.QLayout:
-	return _Column(*args, **kwargs).generate()
+def Col(*controls) -> QtWidgets.QLayout:
+	layout = QtWidgets.QVBoxLayout()
+	layout.setAlignment(QtCore.Qt.AlignTop)
+	_formset_layout(layout, controls)
+	return layout
 
 
-class _Tabs(BaseLayout):
-	def __init__(self, tabs_dict: dict, **kwargs):
-		BaseLayout.__init__(self, **kwargs)
-		self.tabs_dict = tabs_dict
-
-	def generate(self, parent=None, children_controls:list = None) -> QtWidgets.QWidget:
-		def layout_box(_content):
-			if isinstance(_content, QtWidgets.QLayout):
-				return _content
-			# make a Column containing the content
-			return Col(_content)
-
-		tabs = QtWidgets.QTabWidget(parent)
-		for label, content in self.tabs_dict.items():
-			layout = layout_box(content)
-			layout.setMargin(TAB_PADDING)
-			tabs.addTab(
-				ensure_widget(layout),
-				label)
-		return tabs
-
-def Tabs(*args, **kwargs) -> QtWidgets.QTabWidget:
-	return _Tabs(*args, **kwargs).generate()
-
+def Tabs(tabs_dict: dict) -> QtWidgets.QTabWidget:
+	tabs = QtWidgets.QTabWidget()
+	for label, content in tabs_dict.items():
+		layout = ensure_layout(content)
+		layout.setMargin(TAB_PADDING)
+		tabs.addTab(
+			ensure_widget(layout),
+			label)
+	return tabs
 
 
 def Grid(*controls, columns=3):
