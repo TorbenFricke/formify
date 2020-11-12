2b7d5f3d152c21c9b9dc4d9fc58becd2ee27f05b
Torben Fricke <torben.fricke@ial.uni-hannover.de>
2020-11-12 14:50:46 +0100

    disable all variables

--- formify/tools/__init__.py
+++ formify/tools/__init__.py
@@ -2,11 +2,4 @@ from formify.tools.background_method import BackgroundMethod, Task, do_in_ui_thr
 from formify.tools.relationship import Relationship
 from formify.tools.file_dialogs import save_dialog, open_dialog
 from formify.tools.dialogs import *
-from formify.layout import ensure_widget
-from PySide2 import QtWidgets
-
-
-def maximize(widget) -> QtWidgets.QWidget:
-	widget = ensure_widget(widget)
-	widget.setSizePolicy(QtWidgets.QSizePolicy.Expanding, QtWidgets.QSizePolicy.Expanding)
-	return widget
+from formify.tools.misc import *
---
+++ formify/tools/misc.py
@@ -0,0 +1,31 @@
+from formify.layout import ensure_widget
+from PySide2 import QtWidgets
+
+
+def maximize(widget) -> QtWidgets.QWidget:
+	widget = ensure_widget(widget)
+	widget.setSizePolicy(QtWidgets.QSizePolicy.Expanding, QtWidgets.QSizePolicy.Expanding)
+	return widget
+
+
+def disable_all_variables(widget):
+	# import here to prevent circular imports
+	from formify.controls import ValueMixin, Form, ListForm
+
+	for child in widget.children():
+
+		# not a widget
+		if not isinstance(child, QtWidgets.QWidget):
+			continue
+
+		# all normal controls
+		if isinstance(child, ValueMixin) and not isinstance(child, Form):
+			if child.variable_name:
+				child.setEnabled(False)
+
+		# hide add and remove buttons on list control
+		if isinstance(child, ListForm):
+			child.control.add_button.hide()
+			child.control.remove_button.hide()
+
+		disable_all_variables(child)
