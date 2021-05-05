8c8b229af5254adee440774254b969583f3cace7
t.fricke <torben.fricke@ial.uni-hannover.de>
2021-05-05 13:20:15 +0200

    conditional form bug fix

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 setup(
     name="formify",
-    version="1.1.2",
+    version="1.1.3",
     packages=find_packages(),
 	package_data={
         "": ["*.css"],
