aa0e986074bab38ba14fc0e6604ef7f4e8fc68d3
torben <mail@torben.co>
2022-02-11 08:02:54 +0100

    starting on docs

---
+++ docs/form.md
@@ -0,0 +1,12 @@
+# Localization
+
+There are two...
+
+## Translator
+###### ::: formify.localization.Translator
+    rendering:
+      show_root_heading: true
+    
+###### ::: formify.localization.default_translator
+
+## Simple Language Switch
--- formify/localization.py
+++ formify/localization.py
@@ -2,34 +2,67 @@ import json
 
 
 class Translator:
-	def __init__(self, language:str=None):
+
+	def __init__(self, language: str = None):
+		"""
+		By the default, the system language is used (i.e. "en", "de", "fr", ...)
+		"""
 		# get the systems default language
 		if language is None:
 			import locale
 			language = locale.getdefaultlocale()[0].split("_")[0]
 		self.language = language
+		"""Set the current language (usually a language code)"""
 		self.translations = {}
 
-	def add(self, id, **langauges):
+	def add(self, id: str, **langauges):
+		"""
+		Add translations for an id. You can use any string as `id.
+
+		```
+		translator = Translator()
+		translator.add("file", en="file", de="Datei"})
+
+		translator.language = "de"
+		print(translator("file")) # returns "Datei"
+		```
+		"""
 		self.translations[id] = langauges
 		return self(id)
 
-	def __call__(self, id):
+	def __call__(self, id: str):
+		"""
+		Grab the translation for id based on the current language. Always returns a string.
+		If no translation for the current language `translator.language` is provided, the id is returned.
+
+		```
+		translator("file_name")
+		```
+		"""
 		# grab the specified id, or an empty dict
 		translations = self.translations.get(id, {})
 		# return id, if the language was not found
 		return translations.get(self.language, id)
 
-	def load(self, file_name):
+	def load(self, file_name: str):
+		"""
+		Reads all translations into a JSON file.
+		"""
 		with open(file_name) as f:
 			self.translations.update(json.load(f))
 
-	def save(self, file_name):
+	def save(self, file_name: str):
+		"""
+		Dumps all translations into a JSON file.
+		"""
 		with open(file_name, "w+") as f:
 			json.dump(self.translations, f, indent=2, sort_keys=True)
 
 
 def default_translator(*args, **kwargs) -> Translator:
+	"""
+	Returns a `Translator`, prepopulated with a few default german english translations.
+	"""
 	translator = Translator(*args, **kwargs)
 	translator.translations.update({
 		"Open...": {"de": "Öffnen..."},
